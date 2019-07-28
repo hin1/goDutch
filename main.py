c70288ab8e8c8e30c7f04facd8cecb45f9930a83
Daniel <daniel.c.y.cyc@gmail.com>
2019-07-28 17:15:35 +0800

    token changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 from telegram.ext import *
 from telegram import *
 
-TOKEN = TOKEN
+#TOKEN = TOKEN
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN,use_context=True)
 # add handlers
